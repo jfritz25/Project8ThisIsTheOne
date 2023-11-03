299ace70ac741d9c0808b861ac5b63e56a43e712
jamfritz <jamfritz>
2023-11-03 13:31:35 -0400

    Added glide, need implict intent

--- app/src/main/java/com/example/project8/DisplayFragment.kt
+++ app/src/main/java/com/example/project8/DisplayFragment.kt
@@ -1,21 +1,49 @@
 package com.example.project8
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.project8.databinding.FragmentSearchBinding
 
 class DisplayFragment: Fragment() {
     val movie = DisplayFragmentArgs.fromBundle(requireArguments()).Movie
 
-    fun bind(){
-        val genre = movie.Genre
-        val imdbRating = movie.imdbRating
-        val title = movie.Title
-        val year = movie.Year
-        val rating = movie.Rated
-        val runtime = movie.Runtime
-        val poster = movie.Poster
+    private var _binding: FragmentSearchBinding? = null
+
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = binding.root
+        binding.Genre.text = movie.Genre
+        binding.imdbRating.text = movie.imdbRating
+        binding.Title.text = movie.Title
+        binding.Year.text = movie.Year
+        binding.Rated.text = movie.Rated
+        binding.Runtime.text = movie.Runtime
+
+        Glide.with(requireContext()).load(movie.Poster)
+            .apply(
+                RequestOptions().transform(
+                    CenterCrop(), RoundedCorners(20)
+                )
+            )
+            .into(binding.Poster)
+
+        return view
+
     }
 
 
 
 }
+
